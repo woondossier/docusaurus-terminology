6923f899607b9c5071fc6011ec94bf64e27b831b
Thanasis Katsadas <kathan@admin.grnet.gr>
2020-12-03 14:24:19 +0200

    Add correct edit url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -86,9 +86,10 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
+          showLastUpdateTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://gitlab.grnet.gr/devs/docusaurus-terminology/-/tree/master/website',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
